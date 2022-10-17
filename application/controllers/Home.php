3e0f58d77ff060fa6177f2792661f3c724c1c158
Irfan <ip.5090@gmail.com>
2022-10-17 16:10:29 +0800

    upload pertama

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home extends CI_Controller {
+
+    function index()
+    {
+        echo 'Belajar WEB Framework';
+    }
+
+    public function tampil()
+    {
+        return $this->load->view('v_tampil');
+    }
+
+}
---
+++ application/views/v_tampil.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Tampil</title>
+</head>
+<body>
+	<h1>Belajar</h1>
+</body>
+</html>
